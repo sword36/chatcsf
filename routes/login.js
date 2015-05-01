723f493a98e73b74a0210c5bf23b93ae80611ab5
sword36 <sword36rus@gmail.com>
2015-05-01 20:16:58 +0300

    tap to private

--- routes/login.js
+++ routes/login.js
@@ -10,7 +10,17 @@ exports.get = function (req, res) {
 exports.post = function(req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
+    var regUser = /^([a-zа-яё0-9_]{3,20})$/i;
+    var regPas = /^.{4,12}$/;
     User.authorize(username, password, function(err, user) {
+        if (!regUser.test(username))
+        {
+            return next(new HttpError(403, "Недопустимый логин"));
+        }
+        if (!regPas.test(password))
+        {
+            return next(new HttpError(403, "Пароль должен содержать от 4 до 12 символов"))
+        }
         if (err)
         {
             if (err instanceof AuthError){
