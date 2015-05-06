dfa5d4bf66e1d2bbdfc1f82a924209fd269cec66
sword36 <sword36rus@gmail.com>
2015-05-06 15:59:58 +0300

    link to img and bg color

--- socket/index.js
+++ socket/index.js
@@ -76,7 +76,7 @@ module.exports = function(server) {
             callback(err);
         })
     });
-    //io.set("origins", "localhost:*");
+      //io.set("origins", "localhost:*");
      io.set("origins", "http://www.chatcsf.ru:*");
 
 
